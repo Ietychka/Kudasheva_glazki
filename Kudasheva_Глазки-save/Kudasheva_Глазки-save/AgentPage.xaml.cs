5e29290f522a64e1fc5a53445c614bf7c89fdc9c
1ravil <RavilIbakov102@yandex.ru>
2023-10-25 18:29:34 +0500

    11 лаба

--- Kudasheva_Глазки-save/Kudasheva_Глазки-save/AgentPage.xaml.cs
+++ Kudasheva_Глазки-save/Kudasheva_Глазки-save/AgentPage.xaml.cs
@@ -23,11 +23,94 @@ namespace Kudasheva_Глазки_save
         public AgentPage()
         {
             InitializeComponent();
+            var currentAgent = Kudasheva_glazkiEntities.GetContext().Agent.ToList();
+            AgentListView.ItemsSource = currentAgent;
+            ComboType.SelectedIndex = 0;
+            TipType.SelectedIndex = 0;
+            UpdateAgent();
+        }
+
+         private void UpdateAgent()
+        {         
+            var currentAgent = Kudasheva_glazkiEntities.GetContext().Agent.ToList();
+            if(ComboType.SelectedIndex == 0)
+            {
+                currentAgent = currentAgent.OrderBy(p => p.Title).ToList();
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentAgent = currentAgent.OrderBy(p => p.Title).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentAgent = currentAgent.OrderByDescending(p => p.Title).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentAgent = currentAgent.OrderBy(p => p.Title).ToList();
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentAgent = currentAgent.OrderBy(p => p.Title).ToList();
+            }
+            if (ComboType.SelectedIndex == 5)
+            {
+                currentAgent = currentAgent.OrderBy(p => p.Priority).ToList();
+            }
+            if (ComboType.SelectedIndex == 6)
+            {
+                currentAgent = currentAgent.OrderByDescending(p => p.Priority).ToList();
+            }
+            if (TipType.SelectedIndex == 0)
+            {
+                currentAgent = currentAgent;
+            }
+            if (TipType.SelectedIndex == 1)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "МФО").ToList();
+            }
+            if (TipType.SelectedIndex == 2)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ООО").ToList();
+            }
+            if (TipType.SelectedIndex == 3)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ЗАО").ToList();
+            }
+            if (TipType.SelectedIndex == 4)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "МКК").ToList();
+            }
+            if (TipType.SelectedIndex == 5)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ОАО").ToList();
+            }
+            if (TipType.SelectedIndex == 6)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ПАО").ToList();
+            }
+            currentAgent = currentAgent.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            AgentListView.ItemsSource = currentAgent;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate( new AddEditPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAgent();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgent();
+        }
+
+        private void TipType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgent();
         }
     }
 }
